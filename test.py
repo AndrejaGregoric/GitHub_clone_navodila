f7c7fa1194f19bcb6390d11041c41196e1738b68
AndrejaGregoric <46558971+AndrejaGregoric@users.noreply.github.com>
2019-06-06 23:11:07 +0200

    Update navodil

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-dfghfgjhfj
+"""
+Obstaja še hitrejši način kreiranja GIT arhiva. Koraki so naslednji:
+
+V navigacijski vrstici, poleg svojega uporabniškega imena (desno), klikni na + (plus) in izberi New repository.
+Projektu daj neko ime (glej da ne bo presledka vmes).
+Nujno obkljukaj Initialize this repository with a README.
+Klikni na Create repository.
+V desnem stolpcu, pod HTTPS clone URL kopiraj to povezavo.
+Odpri Git Shell na svojem računalniku ter vanj vpiši ukaz git clone ter URL, ki si ga kopiral v prejšnjem koraku. Izgledati mora približno tako: git clone https://github.com/ramuta/test.git
+Ustvarila se ti je mapa projekta, v kateri je že git arhiv ter dodan git remote url. V tej mapi zdaj že lahko nekaj spremeniš, ter uporabiš ukaze add, commit in push (kot opisani zgoraj).
+
+"""
